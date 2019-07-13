37e9e3243362e309c5facdc3a55905866d32cf7a
gabrielbezerra81 <gabriel_alencar2@hotmail.com>
2019-07-13 13:28:40 -0300

    6 formularios prontos: agendada, limitada, mercado, start stop e book

--- src/components/forms/compra_StartStop/GraficoCompraStartStop.js
+++ src/components/forms/compra_StartStop/GraficoCompraStartStop.js
@@ -8,7 +8,7 @@ import {
   mudarStopExecAction
 } from "../../redux/actions/formInputActions";
 import IconeConfigGrafico from "../../utils/IconeConfigGrafico";
-import graficoCompraStartStop from "../../../img/compraStartStop.PNG";
+import graficoCompraStartStop from "../../../img/CompraModeloNovo.png";
 import {
   LabelInputGrafico,
   TextoValorTotalGrafico,
