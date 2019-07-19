b724496a3588e40d37a968a63dfe1946517602f8
gabrielbezerra81 <gabriel_alencar2@hotmail.com>
2019-07-18 22:18:13 -0300

    mostra preço apenas se preencher os inputs

--- src/components/utils/CalculoValorTotal.js
+++ src/components/utils/CalculoValorTotal.js
@@ -1,33 +1,44 @@
 export const CalculoValorTotalAgendada = (disparo, execucao, qtde) => {
   let total = qtde * execucao;
-  let stringValorTotal = "VALOR TOTAL: R$ ";
-  if (
-    execucao === "0.00" ||
-    execucao === 0 ||
-    execucao === "0" ||
-    (execucao === "" && disparo)
-  ) {
-    total = qtde * disparo;
-    stringValorTotal = "VALOR APROXIMADO: R$ ";
+  let stringValorTotal = "";
+
+  if (qtde !== "") {
+    if (execucao === "" && disparo !== "") {
+      total = qtde * disparo;
+      stringValorTotal = "VALOR APROXIMADO: R$ ";
+      return stringValorTotal + Number(total).toFixed(2);
+    }
+    if (execucao !== "") {
+      stringValorTotal = "VALOR TOTAL: R$ ";
+      return stringValorTotal + Number(total).toFixed(2);
+    }
   }
 
-  return stringValorTotal + Number(total).toFixed(2);
+  return "";
 };
 
 export const CalculoValorTotalLimitada = (preco, qtde) => {
-  while (preco.search(",") !== -1) {
-    preco = preco.replace(",", "");
-  }
-  let total = qtde * Number(preco);
+  if (preco !== "" && qtde !== "") {
+    while (preco.search(",") !== -1) {
+      preco = preco.replace(",", "");
+    }
+    let total = qtde * Number(preco);
 
-  return "VALOR TOTAL: R$ " + Number(total).toFixed(2);
+    return "VALOR TOTAL: R$ " + Number(total).toFixed(2);
+  }
+
+  return "";
 };
 
 export const CalculoValorAproximadoMercado = (qtde, cotacaoAtual) => {
-  while (cotacaoAtual.search(",") !== -1) {
-    cotacaoAtual = cotacaoAtual.replace(",", ".");
-  }
-  const total = qtde * Number(cotacaoAtual);
+  if (cotacaoAtual !== "" && qtde !== "") {
+    while (cotacaoAtual.search(",") !== -1) {
+      cotacaoAtual = cotacaoAtual.replace(",", ".");
+    }
+    const total = qtde * Number(cotacaoAtual);
 
-  return "VALOR ESTIMADO: R$" + Number(total).toFixed(2);
+    return "VALOR ESTIMADO: R$" + Number(total).toFixed(2);
+  }
+
+  return "";
 };
