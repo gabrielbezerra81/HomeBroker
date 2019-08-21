492de9306cadffa2de9ea566a0d56ea1eef110db
gabrielbezerra81 <gabriel_alencar2@hotmail.com>
2019-08-21 14:11:41 -0300

    Correção em espaços vazios estarem ocupando espaço no formulário de ordens em execução e impedindo cliques em formulários atrá dele.

--- src/components/forms/ordens_em_execucao/OrdensExecucao.js
+++ src/components/forms/ordens_em_execucao/OrdensExecucao.js
@@ -197,27 +197,5 @@ const dataOrdensExecucao = [
     roteador: "MT5",
     st: ["Ok", "Ok"],
     msg: ""
-  },
-  {
-    progresso: "0%",
-    cadastro: "16/09/2019 13:12:67",
-    corretora: "XP",
-    conta: "23897-8",
-    operacao: "THL",
-    modoExec: "A Mercado",
-    ativo: ["PETRG260", "PETRH270"],
-    oferta: ["V", "C"],
-    qtdeOferta: ["1.000", "1.000"],
-    qtdeEmAberto: ["1.000", "1.000"],
-    qtdeExecutada: [0, 0],
-    qtdeCancelada: [0, 0],
-    precoDisparo: "0,20",
-    precoEnvio: ["2,70", "2,50"],
-    precoExecutado: ["0,20", "0,20"],
-    cotacao: "",
-    validade: "",
-    roteador: "MT5",
-    st: ["Ok", "Ok"],
-    msg: ""
   }
 ];
