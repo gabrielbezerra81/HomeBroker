6f39966b662cea27d6e875f7a2b04afb6b37a575
gabrielbezerra81 <gabriel_alencar2@hotmail.com>
2019-08-27 00:49:48 -0300

    Lista completa: criada a div do topo que permite selecionar opções de visualização e ordenação da lista.

--- src/components/forms/lista_completa/ListaCompleta.js
+++ src/components/forms/lista_completa/ListaCompleta.js
@@ -1,8 +1,9 @@
 import React from "react";
 import "react-datepicker/dist/react-datepicker.css";
-import {} from "react-bootstrap";
+import { Row, Col, Form } from "react-bootstrap";
 import DraggableModal from "components/utils/DraggableModal";
 import { modalHeaderSemBook } from "components/utils/FormHeader";
+import { iconeConfigAbrirFormulario } from "components/utils/IconesConfigFormInterno";
 
 export default class ListaCompleta extends React.Component {
   componentDidMount() {
@@ -34,6 +35,68 @@ export default class ListaCompleta extends React.Component {
   */
 
   modalBody = props => {
-    return <div className="bodyMultileg"></div>;
+    return (
+      <div className="bodyListaCompleta">
+        <Row>
+          <Col md={2}>
+            <h6>GRIDI</h6>
+          </Col>
+          <Col md={2}>
+            <div className="divIconeConfigOrdernar">
+              <h6 className="mr-2">Ordernar</h6>
+              {iconeConfigAbrirFormulario(() => false, "")}
+            </div>
+          </Col>
+        </Row>
+        <Row className="rowHeaderListaCompleta mt-2">
+          <Col>
+            <Form.Group>
+              <Form.Control as="select" className="textInput formPosicao">
+                <option value="posicao">Posição</option>
+              </Form.Control>
+            </Form.Group>
+          </Col>
+
+          <Col>
+            <div className="divSeletor">
+              <div tabIndex={0} className="divClicavel seletorAtivo">
+                <h6>ORIGINAL</h6>
+              </div>
+              <div tabIndex={0} className="divClicavel seletorAtivo">
+                <h6>SÍMBOLO</h6>
+              </div>
+              <div tabIndex={0} className="divClicavel seletorAtivo">
+                <h6>LÍQUIDO</h6>
+              </div>
+              <div tabIndex={0} className="divClicavel seletorAtivo">
+                <h6>ÚLTIMO</h6>
+              </div>
+              <div tabIndex={0} className="divClicavel seletorAtivo">
+                <h6>RANK</h6>
+              </div>
+              <div tabIndex={0} className="divClicavel seletorAtivo">
+                <h6>IMPL VOL</h6>
+              </div>
+              <div tabIndex={0} className="divClicavel seletorAtivo">
+                <h6>GANHOS ENCONTRO</h6>
+              </div>
+            </div>
+          </Col>
+          <Col>
+            <div className="divSeletor">
+              <div tabIndex={0} className="divClicavel seletorAtivo">
+                <h6>ALTA</h6>
+              </div>
+              <div tabIndex={0} className="divClicavel seletorAtivo">
+                <h6>PEQUENO</h6>
+              </div>
+              <div tabIndex={0} className="divClicavel seletorAtivo">
+                <h6>LISTA</h6>
+              </div>
+            </div>
+          </Col>
+        </Row>
+      </div>
+    );
   };
 }
