e96cad6dac2b922d8570f3987d045117b919326f
gabrielbezerra81 <gabriel_alencar2@hotmail.com>
2019-09-17 13:43:50 -0300

    Book do multileg finalizado.

--- src/components/forms/multileg_/AbaMultileg.js
+++ src/components/forms/multileg_/AbaMultileg.js
@@ -1,23 +1,12 @@
 import React from "react";
 import { Table, Form, Button, InputGroup, Col, Row } from "react-bootstrap";
-import imgModeloEU from "img/modeloEU.png";
-import imgModeloUSA from "img/modeloUSA.png";
 import DatePicker from "react-datepicker";
 import { ReactComponent as ArrowDown } from "img/down-arrow.svg";
 import { ReactComponent as ArrowUp } from "img/up-arrow.svg";
-import { MDBCol, MDBIcon } from "mdbreact";
-
-const capitalize = function(str) {
-  return str.charAt(0).toUpperCase() + str.slice(1);
-};
-/*<Form.Group>
-  <Form.Control
-    className="inputAtivo"
-    type="text"
-    value="PETR4"
-    onChange={() => false}
-  />
-</Form.Group>; */
+import { MDBIcon } from "mdbreact";
+import TabelaMultileg from "components/forms/multileg_/TabelaMultileg";
+import IconeConfigGrafico from "components/utils/IconeConfigGrafico";
+import { iconeConfigAbrirFormulario } from "components/utils/IconesConfigFormInterno";
 
 class AbaMultileg extends React.Component {
   render() {
@@ -82,118 +71,18 @@ class AbaMultileg extends React.Component {
               </div>
             </div>
           </div>
-          <Table
-            variant="dark"
-            bordered={false}
-            borderless
-            striped
-            className="tableMultileg text-center"
-            style={{ tableLayout: "auto" }}
-          >
-            <thead>
-              <tr>
-                <th>C/V</th>
-                <th>Qtde.</th>
-                <th>Série</th>
-                <th>Strike</th>
-                <th>Código</th>
-                <th>Tipo</th>
-                <th>Modelo</th>
-                <th>Despernamento Máx.</th>
-                <th>Prioridade de Execução</th>
-                <th>Cotação</th>
-                <th>Valor</th>
-              </tr>
-            </thead>
-            <tbody>
-              {tabelaMultileg.map((item, index) => {
-                return (
-                  <tr key={index}>
-                    {renderCV(item.cv)}
-                    <td>
-                      <Form.Group>
-                        <Form.Control
-                          className="textInput formDespernamento "
-                          type="number"
-                          min={0}
-                          step={100}
-                          //value={item.qtde}
-                        />
-                      </Form.Group>
-                    </td>
-                    <td>
-                      <Form.Group>
-                        <Form.Control
-                          as="select"
-                          className="textInput"
-                          //value={item.prioridade}
-                        >
-                          {item.serie.map((serie, indice) => (
-                            <option key={serie + indice} value={serie}>
-                              {serie}
-                            </option>
-                          ))}
-                        </Form.Control>
-                      </Form.Group>
-                    </td>
-                    <td>
-                      <Form.Group>
-                        <Form.Control
-                          as="select"
-                          className="textInput"
-                          //value={item.prioridade}
-                        >
-                          {item.strike.map((strike, indice) => (
-                            <option key={strike + indice} value={strike}>
-                              {strike}
-                            </option>
-                          ))}
-                        </Form.Control>
-                      </Form.Group>
-                    </td>
-                    <td>{item.codigo}</td>
-                    <td>{capitalize(item.tipo)}</td>
-                    {renderModelo(item.modelo)}
-                    <td>
-                      <Form.Group>
-                        <Form.Control
-                          className="textInput formDespernamento"
-                          type="number"
-                          min={0}
-                          step={100}
-                          //value={item.despernamento}
-                        />
-                      </Form.Group>
-                    </td>
-                    <td>
-                      <Form.Group>
-                        <Form.Control
-                          as="select"
-                          className="textInput formPrioridade"
-                          //value={item.prioridade}
-                        >
-                          <option value={-1}>-1</option>
-                          <option value={0}>0</option>
-                          <option value={1}>1</option>
-                          <option value={2}>2</option>
-                          <option value={3}>3</option>
-                          <option value={4}>4</option>
-                        </Form.Control>
-                      </Form.Group>
-                    </td>
-                    <td>{item.cotacao}</td>
-                    <td>{item.valor}</td>
-                  </tr>
-                );
-              })}
-            </tbody>
-          </Table>
+          <TabelaMultileg></TabelaMultileg>
         </div>
         <div>
           <Row>
             <Col className="text-align-center mb-2">
               <h6>Book</h6>
             </Col>
+            <IconeConfigGrafico
+              handleShow={() => false}
+              name="config_complementar"
+              id="icone_config_complementar"
+            />
           </Row>
           <Row>
             <Col className="text-align-center">
@@ -236,13 +125,34 @@ class AbaMultileg extends React.Component {
               </Table>
             </Col>
           </Row>
+          <Row className="ml-3 mr-1 rowTextoInputRange">
+            <Col md={4}>
+              <span>Min</span>
+            </Col>
+            <Col md={4}>
+              <span>Med</span>
+            </Col>
+            <Col md={4}>
+              <span>Max</span>
+            </Col>
+          </Row>
           <Row className="ml-3 mr-3">
-            <Col>
+            <Col className="colInputRange">
               <input
                 type="range"
                 className={`custom-range inputRangeMultileg`}
-                //value={item.valorAcao}
               />
+            </Col>
+          </Row>
+          <Row className="ml-3 mr-1 mb-2 rowTextoInputRange">
+            <Col md={4}>
+              <span>0,80</span>
+            </Col>
+            <Col md={4}>
+              <span>0,90</span>
+            </Col>
+            <Col md={4}>
+              <span>1,00</span>
             </Col>
           </Row>
           <Row className="mr-2 mb-2">
@@ -307,40 +217,6 @@ class AbaMultileg extends React.Component {
 
 export default AbaMultileg;
 
-const renderCV = cv => {
-  return (
-    <td>
-      {cv === "compra" ? (
-        <div className="divCV">
-          <h6 className="cvCompra"> C </h6>
-          <h6> V </h6>
-        </div>
-      ) : (
-        <div className="divCV">
-          <h6> C </h6>
-          <h6 className="cvVenda"> V </h6>
-        </div>
-      )}
-    </td>
-  );
-};
-
-const renderModelo = modelo => {
-  return (
-    <td>
-      {modelo === "EU" ? (
-        <div>
-          <img src={imgModeloEU} alt="" className="imgModelo" />
-        </div>
-      ) : (
-        <div>
-          <img src={imgModeloUSA} alt="" className="imgModelo" />
-        </div>
-      )}
-    </td>
-  );
-};
-
 const renderSeta = valor => {
   valor = Number(valor);
   if (valor >= 0) return <ArrowUp fill="#138342" className="mr-1" width="35" />;
@@ -365,32 +241,3 @@ const renderValorPorcentagem = porcentagem => {
     return <h6>+{porcentagem}%</h6>;
   }
 };
-
-const tabelaMultileg = [
-  {
-    cv: "compra",
-    qtde: 1000,
-    serie: ["2019-08", "2019-07", "2019-06"],
-    strike: [26.32, 27.48, 28.48],
-    codigo: "PETRH275",
-    tipo: "call",
-    modelo: "EU",
-    despernamento: 100,
-    prioridade: -1,
-    cotacao: "15,25",
-    valor: "-40,00"
-  },
-  {
-    cv: "venda",
-    qtde: 2000,
-    serie: ["2019-08", "2019-07", "2019-06"],
-    strike: [26.32, 27.48, 28.48],
-    codigo: "PETRH275",
-    tipo: "call",
-    modelo: "USA",
-    despernamento: 500,
-    prioridade: 2,
-    cotacao: "10,30",
-    valor: "-200,00"
-  }
-];
---
+++ src/components/forms/multileg_/TabelaMultileg.js
@@ -0,0 +1,184 @@
+import React from "react";
+import { Table, Form } from "react-bootstrap";
+import imgModeloEU from "img/modeloEU.png";
+import imgModeloUSA from "img/modeloUSA2.svg";
+
+const capitalize = function(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+};
+
+export default class TabelaMultileg extends React.Component {
+  render() {
+    return (
+      <Table
+        variant="dark"
+        bordered={false}
+        borderless
+        striped
+        className="tableMultileg text-center"
+        style={{ tableLayout: "auto" }}
+      >
+        <thead>
+          <tr>
+            <th>C/V</th>
+            <th>Qtde.</th>
+            <th>Série</th>
+            <th>Strike</th>
+            <th>Código</th>
+            <th>Tipo</th>
+            <th>Modelo</th>
+            <th>Despernamento Máx.</th>
+            <th>Prioridade de Execução</th>
+            <th>Cotação</th>
+            <th>Valor</th>
+          </tr>
+        </thead>
+        <tbody>
+          {tabelaMultileg.map((item, index) => {
+            return (
+              <tr key={index}>
+                {renderCV(item.cv)}
+                <td>
+                  <Form.Group>
+                    <Form.Control
+                      className="textInput formDespernamento "
+                      type="number"
+                      min={0}
+                      step={100}
+                      //value={item.qtde}
+                    />
+                  </Form.Group>
+                </td>
+                <td>
+                  <Form.Group>
+                    <Form.Control
+                      as="select"
+                      className="textInput"
+                      //value={item.prioridade}
+                    >
+                      {item.serie.map((serie, indice) => (
+                        <option key={serie + indice} value={serie}>
+                          {serie}
+                        </option>
+                      ))}
+                    </Form.Control>
+                  </Form.Group>
+                </td>
+                <td>
+                  <Form.Group>
+                    <Form.Control
+                      as="select"
+                      className="textInput"
+                      //value={item.prioridade}
+                    >
+                      {item.strike.map((strike, indice) => (
+                        <option key={strike + indice} value={strike}>
+                          {strike}
+                        </option>
+                      ))}
+                    </Form.Control>
+                  </Form.Group>
+                </td>
+                <td>{item.codigo}</td>
+                <td>{capitalize(item.tipo)}</td>
+                {renderModelo(item.modelo)}
+                <td>
+                  <Form.Group>
+                    <Form.Control
+                      className="textInput formDespernamento"
+                      type="number"
+                      min={0}
+                      step={100}
+                      //value={item.despernamento}
+                    />
+                  </Form.Group>
+                </td>
+                <td>
+                  <Form.Group>
+                    <Form.Control
+                      as="select"
+                      className="textInput formPrioridade"
+                      //value={item.prioridade}
+                    >
+                      <option value={-1}>-1</option>
+                      <option value={0}>0</option>
+                      <option value={1}>1</option>
+                      <option value={2}>2</option>
+                      <option value={3}>3</option>
+                      <option value={4}>4</option>
+                    </Form.Control>
+                  </Form.Group>
+                </td>
+                <td>{item.cotacao}</td>
+                <td>{item.valor}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </Table>
+    );
+  }
+}
+
+const renderCV = cv => {
+  return (
+    <td>
+      {cv === "compra" ? (
+        <div className="divCV">
+          <h6 className="cvCompra"> C </h6>
+          <h6> V </h6>
+        </div>
+      ) : (
+        <div className="divCV">
+          <h6> C </h6>
+          <h6 className="cvVenda"> V </h6>
+        </div>
+      )}
+    </td>
+  );
+};
+
+const renderModelo = modelo => {
+  return (
+    <td>
+      {modelo === "EU" ? (
+        <div>
+          <img src={imgModeloEU} alt="" className="imgModelo" />
+        </div>
+      ) : (
+        <div>
+          <img src={imgModeloUSA} alt="" className="imgModelo" />
+        </div>
+      )}
+    </td>
+  );
+};
+
+const tabelaMultileg = [
+  {
+    cv: "compra",
+    qtde: 1000,
+    serie: ["2019-08", "2019-07", "2019-06"],
+    strike: [26.32, 27.48, 28.48],
+    codigo: "PETRH275",
+    tipo: "call",
+    modelo: "EU",
+    despernamento: 100,
+    prioridade: -1,
+    cotacao: "15,25",
+    valor: "-40,00"
+  },
+  {
+    cv: "venda",
+    qtde: 2000,
+    serie: ["2019-08", "2019-07", "2019-06"],
+    strike: [26.32, 27.48, 28.48],
+    codigo: "PETRH275",
+    tipo: "call",
+    modelo: "USA",
+    despernamento: 500,
+    prioridade: 2,
+    cotacao: "10,30",
+    valor: "-200,00"
+  }
+];
