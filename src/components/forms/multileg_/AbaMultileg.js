7557954d8c7c9d9191ede36fd3b074d3ccf7c3ab
gabrielbezerra81 <gabriel_alencar2@hotmail.com>
2019-09-17 00:26:33 -0300

    Multileg: tabela de book adicionada

--- src/components/forms/multileg_/AbaMultileg.js
+++ src/components/forms/multileg_/AbaMultileg.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Table, Form, Button, InputGroup } from "react-bootstrap";
+import { Table, Form, Button, InputGroup, Col, Row } from "react-bootstrap";
 import imgModeloEU from "img/modeloEU.png";
 import imgModeloUSA from "img/modeloUSA.png";
 import DatePicker from "react-datepicker";
@@ -23,170 +23,220 @@ class AbaMultileg extends React.Component {
   render() {
     return (
       <div className="containerAbaMultileg">
-        <div className="divDetalhesAbaMultileg">
-          <div className="divColunaDetalhes">
-            <InputGroup>
-              <Form.Control
-                className="inputAtivo"
-                type="text"
-                value="PETR4"
-                onChange={() => false}
-              />
-              <InputGroup.Append className="inputAtivoAppend">
-                <span className="input-group-text iconeProcurar divClicavel">
-                  <MDBIcon icon="search" />
-                </span>
-              </InputGroup.Append>
-            </InputGroup>
-            <h5 className="textoValor">-27,43</h5>
-            {renderSeta(2)}
-            {renderValorPorcentagem(5.35)}
-          </div>
-          <div className="divColunaDetalhes">
-            <Form.Group>
-              <Form.Label>Strike</Form.Label>
-              <Form.Control
-                className="textInput"
-                type="number"
-                step={0.01}
-                //value={1}
-                onChange={() => false}
-              />
-            </Form.Group>
-            <Form.Group className="wrapperVencimento">
-              <Form.Label>Vencimento</Form.Label>
-              <DatePicker
-                className="form-control textInput"
-                //selected={props.date}
-                onChange={() => false}
-                dateFormat="dd/MM/yyyy"
-                popperPlacement="top-start"
-              ></DatePicker>
-            </Form.Group>
-          </div>
-          <div className="divColunaDetalhes">
-            <div className="divFlexRowDetalhesAba">
-              <Form.Label>Incluir</Form.Label>
-              <div className="botoesIncluir">
-                <Button variant="primary" size="sm">
-                  +Ativo
-                </Button>
-                <Button variant="primary" size="sm">
-                  +Call
-                </Button>
-                <Button variant="primary" size="sm">
-                  +Put
-                </Button>
+        <div>
+          <div className="divDetalhesAbaMultileg">
+            <div className="divColunaDetalhes">
+              <InputGroup>
+                <Form.Control
+                  className="inputAtivo"
+                  type="text"
+                  value="PETR4"
+                  onChange={() => false}
+                />
+                <InputGroup.Append className="inputAtivoAppend">
+                  <span className="input-group-text iconeProcurar divClicavel">
+                    <MDBIcon icon="search" />
+                  </span>
+                </InputGroup.Append>
+              </InputGroup>
+              <h5 className="textoValor">-27,43</h5>
+              {renderSeta(2)}
+              {renderValorPorcentagem(5.35)}
+            </div>
+            <div className="divColunaDetalhes">
+              <Form.Group>
+                <Form.Label>Strike</Form.Label>
+                <Form.Control
+                  className="textInput"
+                  type="number"
+                  step={0.01}
+                  //value={1}
+                  onChange={() => false}
+                />
+              </Form.Group>
+              <Form.Group className="wrapperVencimento">
+                <Form.Label>Vencimento</Form.Label>
+                <DatePicker
+                  className="form-control textInput"
+                  //selected={props.date}
+                  onChange={() => false}
+                  dateFormat="dd/MM/yyyy"
+                  popperPlacement="top-start"
+                ></DatePicker>
+              </Form.Group>
+            </div>
+            <div className="divColunaDetalhes">
+              <div className="divFlexRowDetalhesAba">
+                <Form.Label>Incluir</Form.Label>
+                <div className="botoesIncluir">
+                  <Button variant="primary" size="sm">
+                    +Ativo
+                  </Button>
+                  <Button variant="primary" size="sm">
+                    +Call
+                  </Button>
+                  <Button variant="primary" size="sm">
+                    +Put
+                  </Button>
+                </div>
               </div>
             </div>
           </div>
+          <Table
+            variant="dark"
+            bordered={false}
+            borderless
+            striped
+            className="tableMultileg text-center"
+            style={{ tableLayout: "auto" }}
+          >
+            <thead>
+              <tr>
+                <th>C/V</th>
+                <th>Qtde.</th>
+                <th>Série</th>
+                <th>Strike</th>
+                <th>Código</th>
+                <th>Tipo</th>
+                <th>Modelo</th>
+                <th>Despernamento Máx.</th>
+                <th>Prioridade de Execução</th>
+                <th>Cotação</th>
+                <th>Valor</th>
+              </tr>
+            </thead>
+            <tbody>
+              {tabelaMultileg.map((item, index) => {
+                return (
+                  <tr key={index}>
+                    {renderCV(item.cv)}
+                    <td>
+                      <Form.Group>
+                        <Form.Control
+                          className="textInput formDespernamento "
+                          type="number"
+                          min={0}
+                          step={100}
+                          //value={item.qtde}
+                        />
+                      </Form.Group>
+                    </td>
+                    <td>
+                      <Form.Group>
+                        <Form.Control
+                          as="select"
+                          className="textInput"
+                          //value={item.prioridade}
+                        >
+                          {item.serie.map((serie, indice) => (
+                            <option key={serie + indice} value={serie}>
+                              {serie}
+                            </option>
+                          ))}
+                        </Form.Control>
+                      </Form.Group>
+                    </td>
+                    <td>
+                      <Form.Group>
+                        <Form.Control
+                          as="select"
+                          className="textInput"
+                          //value={item.prioridade}
+                        >
+                          {item.strike.map((strike, indice) => (
+                            <option key={strike + indice} value={strike}>
+                              {strike}
+                            </option>
+                          ))}
+                        </Form.Control>
+                      </Form.Group>
+                    </td>
+                    <td>{item.codigo}</td>
+                    <td>{capitalize(item.tipo)}</td>
+                    {renderModelo(item.modelo)}
+                    <td>
+                      <Form.Group>
+                        <Form.Control
+                          className="textInput formDespernamento"
+                          type="number"
+                          min={0}
+                          step={100}
+                          //value={item.despernamento}
+                        />
+                      </Form.Group>
+                    </td>
+                    <td>
+                      <Form.Group>
+                        <Form.Control
+                          as="select"
+                          className="textInput formPrioridade"
+                          //value={item.prioridade}
+                        >
+                          <option value={-1}>-1</option>
+                          <option value={0}>0</option>
+                          <option value={1}>1</option>
+                          <option value={2}>2</option>
+                          <option value={3}>3</option>
+                          <option value={4}>4</option>
+                        </Form.Control>
+                      </Form.Group>
+                    </td>
+                    <td>{item.cotacao}</td>
+                    <td>{item.valor}</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </Table>
         </div>
-        <Table
-          variant="dark"
-          bordered={false}
-          borderless
-          striped
-          className="tableMultileg text-center"
-          style={{ tableLayout: "auto" }}
-        >
-          <thead>
-            <tr>
-              <th>C/V</th>
-              <th>Qtde.</th>
-              <th>Série</th>
-              <th>Strike</th>
-              <th>Código</th>
-              <th>Tipo</th>
-              <th>Modelo</th>
-              <th>Despernamento Máx.</th>
-              <th>Prioridade de Execução</th>
-              <th>Cotação</th>
-              <th>Valor</th>
-            </tr>
-          </thead>
-          <tbody>
-            {tabelaMultileg.map((item, index) => {
-              return (
-                <tr key={index}>
-                  {renderCV(item.cv)}
-                  <td>
-                    <Form.Group>
-                      <Form.Control
-                        className="textInput formDespernamento "
-                        type="number"
-                        min={0}
-                        step={100}
-                        //value={item.qtde}
-                      />
-                    </Form.Group>
-                  </td>
-                  <td>
-                    <Form.Group>
-                      <Form.Control
-                        as="select"
-                        className="textInput"
-                        //value={item.prioridade}
-                      >
-                        {item.serie.map((serie, indice) => (
-                          <option key={serie + indice} value={serie}>
-                            {serie}
-                          </option>
-                        ))}
-                      </Form.Control>
-                    </Form.Group>
-                  </td>
-                  <td>
-                    <Form.Group>
-                      <Form.Control
-                        as="select"
-                        className="textInput"
-                        //value={item.prioridade}
-                      >
-                        {item.strike.map((strike, indice) => (
-                          <option key={strike + indice} value={strike}>
-                            {strike}
-                          </option>
-                        ))}
-                      </Form.Control>
-                    </Form.Group>
-                  </td>
-                  <td>{item.codigo}</td>
-                  <td>{capitalize(item.tipo)}</td>
-                  {renderModelo(item.modelo)}
-                  <td>
-                    <Form.Group>
-                      <Form.Control
-                        className="textInput formDespernamento"
-                        type="number"
-                        min={0}
-                        step={100}
-                        //value={item.despernamento}
-                      />
-                    </Form.Group>
-                  </td>
-                  <td>
-                    <Form.Group>
-                      <Form.Control
-                        as="select"
-                        className="textInput formPrioridade"
-                        //value={item.prioridade}
-                      >
-                        <option value={-1}>-1</option>
-                        <option value={0}>0</option>
-                        <option value={1}>1</option>
-                        <option value={2}>2</option>
-                        <option value={3}>3</option>
-                        <option value={4}>4</option>
-                      </Form.Control>
-                    </Form.Group>
-                  </td>
-                  <td>{item.cotacao}</td>
-                  <td>{item.valor}</td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </Table>
+        <div>
+          <Row>
+            <Col className="text-align-center mb-2">
+              <h6>Book</h6>
+            </Col>
+          </Row>
+          <Row>
+            <Col className="text-align-center">
+              <h6 className="textoCompra">Compra</h6>
+            </Col>
+            <Col className="text-align-center">
+              <h6 className="textoVenda">Venda</h6>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <Table
+                variant="dark"
+                borderless
+                striped
+                className="tabelaBook text-center"
+              >
+                <thead>
+                  <tr>
+                    <th>Qtde.</th>
+                    <th>Preço</th>
+                    <th>Preço</th>
+                    <th>Qtde.</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>3,7K</td>
+                    <td>2,40</td>
+                    <td>2,50</td>
+                    <td>0,7K</td>
+                  </tr>
+                  <tr>
+                    <td>3,7K</td>
+                    <td>1,50</td>
+                    <td>1,60</td>
+                    <td>1,7K</td>
+                  </tr>
+                </tbody>
+              </Table>
+            </Col>
+          </Row>
+        </div>
       </div>
     );
   }
