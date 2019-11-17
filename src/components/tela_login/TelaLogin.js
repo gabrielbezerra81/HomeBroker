8685f2d5822bfd39c0e74a7b524871c377170af8
gabrielbezerra81 <gabriel_alencar2@hotmail.com>
2019-11-17 18:44:09 -0300

    Imagem de background na tela de login

--- src/components/tela_login/TelaLogin.js
+++ src/components/tela_login/TelaLogin.js
@@ -7,74 +7,80 @@ import {
   mudarDadosLoginAction
 } from "components/redux/actions/TelaPrincipalActions";
 import FloatingLabelInput from "react-floating-label-input";
+import backgrondLogin from "img/backgrounds/background_login.jpg";
 
 class TelaLogin extends React.Component {
   render() {
     const { props } = this;
     return (
-      <div className="containerTelaLogin">
-        <div className="divBemVindo">
-          <h3>Seja bem-vindo!</h3>
-        </div>
-
-        <div className="divLogin">
-          <div className="divTextoLogin mb-4">
-            <h4>Realize seu login para continuar.</h4>
+      <div className="backgroundLogin">
+        <div className="containerTelaLogin">
+          <div className="divBemVindo">
+            <h3>Seja bem-vindo!</h3>
           </div>
-          <Form>
-            <div className="conteudoLogin">
-              <FloatingLabelInput
-                className={`inputFlutuante ${
-                  props.inputUsuario ? "flutuantePreenchido" : ""
-                }`}
-                id="inputUsuarioFlutuante"
-                label="Usuário ou e-mail"
-                value={props.inputUsuario}
-                onChange={e =>
-                  props.mudarDadosLoginAction("inputUsuario", e.target.value)
-                }
-              />
 
-              <FloatingLabelInput
-                className={`inputFlutuante ${
-                  props.inputSenha ? "flutuantePreenchido" : ""
-                }`}
-                id="inputSenhaFlutuante"
-                label="Senha"
-                value={props.inputSenha}
-                onChange={e =>
-                  props.mudarDadosLoginAction("inputSenha", e.target.value)
-                }
-              />
-
-              <Button
-                id="botaoLogar"
-                variant="primary"
-                onClick={() =>
-                  props.logarUsuarioAction(props.inputUsuario, props.inputSenha)
-                }
-              >
-                ACESSAR MINHA CONTA
-              </Button>
-
-              <div className="flexJustifyCenter">
-                <MDBIcon icon="lock" className="mr-1" />
-                <h6 className="ml-1">Esqueci minha senha</h6>
-              </div>
-              <Button variant="primary" className="flexJustifyCenter">
-                <h6 className="ml-1">Cadastre-se</h6>
-              </Button>
+          <div className="divLogin">
+            <div className="divTextoLogin mb-4">
+              <h4>Realize seu login para continuar.</h4>
             </div>
-          </Form>
-        </div>
+            <Form>
+              <div className="conteudoLogin">
+                <FloatingLabelInput
+                  className={`inputFlutuante ${
+                    props.inputUsuario ? "flutuantePreenchido" : ""
+                  }`}
+                  id="inputUsuarioFlutuante"
+                  label="Usuário ou e-mail"
+                  value={props.inputUsuario}
+                  onChange={e =>
+                    props.mudarDadosLoginAction("inputUsuario", e.target.value)
+                  }
+                />
 
-        <div className="divFooter">
-          <div className="mb-4">
-            <h6>Ajuda</h6>
-            <h6>Privacidade</h6>
-            <h6>Termos</h6>
+                <FloatingLabelInput
+                  className={`inputFlutuante ${
+                    props.inputSenha ? "flutuantePreenchido" : ""
+                  }`}
+                  id="inputSenhaFlutuante"
+                  label="Senha"
+                  value={props.inputSenha}
+                  onChange={e =>
+                    props.mudarDadosLoginAction("inputSenha", e.target.value)
+                  }
+                />
+
+                <Button
+                  id="botaoLogar"
+                  variant="primary"
+                  onClick={() =>
+                    props.logarUsuarioAction(
+                      props.inputUsuario,
+                      props.inputSenha
+                    )
+                  }
+                >
+                  ACESSAR MINHA CONTA
+                </Button>
+
+                <div className="flexJustifyCenter">
+                  <MDBIcon icon="lock" className="mr-1" />
+                  <h6 className="ml-1">Esqueci minha senha</h6>
+                </div>
+                <Button variant="primary" className="flexJustifyCenter">
+                  <h6 className="ml-1">Cadastre-se</h6>
+                </Button>
+              </div>
+            </Form>
           </div>
-          <h5>Todos direitos reservados. RENDA CONTINUA @2019</h5>
+
+          <div className="divFooter">
+            <div className="mb-4">
+              <h6>Ajuda</h6>
+              <h6>Privacidade</h6>
+              <h6>Termos</h6>
+            </div>
+            <h5>Todos direitos reservados. RENDA CONTINUA @2019</h5>
+          </div>
         </div>
       </div>
     );
