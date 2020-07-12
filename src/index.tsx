b9f4ed08a8ef687eab82d3fb2c933383ac18e3e9
Gabriel Alencar <gabriel_alencar2@hotmail.com>
2020-07-12 18:24:37 -0300

    Atualização de libs; remoção de libs não utilizadas;

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
-// import LogRocket from "logrocket";
-// import setupLogRocketReact from "logrocket-react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
-import * as serviceWorker from "./serviceWorker";
+// import * as serviceWorker from "./serviceWorker";
 import "@fortawesome/fontawesome-free/css/all.css";
 import { Helper } from "redux/ElementosConectadosRedux";
 import PerfectScrollbar from "react-perfect-scrollbar";
 
-// LogRocket.init("g32h7d/react-homebroker");
-// setupLogRocketReact(LogRocket);
-
 ReactDOM.render(
-  <PerfectScrollbar id="scrollbarPrincipal">{Helper()}</PerfectScrollbar>,
+  <React.StrictMode>
+    <PerfectScrollbar id="scrollbarPrincipal">{Helper()}</PerfectScrollbar>
+  </React.StrictMode>,
   document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// serviceWorker.unregister();
