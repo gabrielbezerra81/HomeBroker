80f4152684fe8a293c5849063a1f8149d27cfaea
Gabriel Alencar <gabriel_alencar2@hotmail.com>
2020-07-16 02:08:44 -0300

    Correção de erros de tipagem no Router

---
+++ src/routing/routes.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Router, Redirect, RouteComponentProps } from "@reach/router";
+import TelaLogin from "components/telas/login/TelaLogin";
+import TelaCadastro from "components/telas/login/TelaCadastro";
+import { useSelectorStorePrincipal } from "redux/StoreCreation";
+import TelaPrincipal from "components/telas/principal/TelaPrincipal";
+
+export const Routes = () => {
+  return (
+    <Router>
+      <TelaLogin path="/" />
+      <TelaCadastro path="/cadastro" />
+      <Home path="/home" />
+    </Router>
+  );
+};
+
+const Home: React.FC<RouteComponentProps> = ({ path }) => {
+  const logado = useSelectorStorePrincipal((state: any) => {
+    return state.telaPrincipalReducer.logado;
+  });
+
+  if (logado) return <TelaPrincipal />;
+
+  return <Redirect to="/" noThrow />;
+};
