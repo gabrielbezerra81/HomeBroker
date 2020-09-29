33fef4b1dc516b774509890c287acbaf98aaa47a
Gabriel Alencar <gabriel_alencar2@hotmail.com>
2020-09-29 12:26:36 -0300

    Adicionando box de cotação a suas respectivas abas na listagem

--- src/screens/home/QuoteBox/QuoteBoxContainer.tsx
+++ src/screens/home/QuoteBox/QuoteBoxContainer.tsx
@@ -1,11 +1,37 @@
-import React from "react";
+import React, { useEffect } from "react";
 import QuoteBox from "./QuoteBox";
 import useStateStorePrincipal from "hooks/useStateStorePrincipal";
+import { listQuoteBoxesAPI } from "api/API";
+import useDispatchStorePrincipal from "hooks/useDispatchStorePrincipal";
+import {
+  handleAddBoxesToTabsAction,
+  updateOneSystemStateAction,
+} from "redux/actions/system/SystemActions";
 
 const QuoteBoxContainer: React.FC = () => {
   const {
     systemReducer: { quoteBoxes },
   } = useStateStorePrincipal();
+
+  const dispatch = useDispatchStorePrincipal();
+
+  useEffect(() => {
+    async function loadBoxes() {
+      const boxes = await listQuoteBoxesAPI();
+
+      const openedBoxes = boxes.map((boxItem: any) => {
+        const configuration = JSON.parse(boxItem.configuration);
+
+        return { menuKey: `box${boxItem.id}`, tabKey: configuration.tabKey };
+      });
+
+      dispatch(handleAddBoxesToTabsAction(openedBoxes));
+
+      // dispatch(updateOneSystemStateAction("quoteBoxes", boxes));
+    }
+
+    loadBoxes();
+  }, [dispatch]);
 
   return (
     <div className="quoteBoxContainer">
