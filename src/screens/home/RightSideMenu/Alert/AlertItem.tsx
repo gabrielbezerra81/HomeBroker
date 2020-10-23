ffaa62d736d28984f758e1623a51c5517dde7c6b
Gabriel Alencar <gabriel_alencar2@hotmail.com>
2020-10-23 16:25:04 -0300

    Menu lateral direito: adicionando botões de header ao alerta

--- src/screens/home/RightSideMenu/Alert/AlertItem.tsx
+++ src/screens/home/RightSideMenu/Alert/AlertItem.tsx
@@ -1,18 +1,36 @@
-import React, { useEffect, useMemo, useState } from "react";
+import React, { useCallback, useEffect, useMemo, useState } from "react";
 import { FaLongArrowAltUp, FaLongArrowAltDown } from "react-icons/fa";
+import { RiCloseCircleFill } from "react-icons/ri";
+import { FiSearch } from "react-icons/fi";
 import moment from "moment";
 import { formatarNumDecimal } from "shared/utils/Formatacoes";
 import { AlertAPI } from "types/multileg/multileg";
+import useDispatchStorePrincipal from "hooks/useDispatchStorePrincipal";
+import { disableAlertAPI } from "api/API";
+import useDispatchGlobalStore from "hooks/useDispatchGlobalStore";
+import {
+  abrirItemBarraLateralAction,
+  updateManySystemState,
+} from "redux/actions/system/SystemActions";
+import { atualizarDivKeyAction } from "redux/actions/GlobalAppActions";
+import useStateStorePrincipal from "hooks/useStateStorePrincipal";
 
 interface AlertItemProps {
   alert: AlertAPI;
 }
 
-const AlertItem: React.FC<AlertItemProps> = ({ alert }) => {
+const AlertItem: React.FC<AlertItemProps> = ({ alert: alertItem }) => {
+  const dispatchGlobal = useDispatchGlobalStore();
+  const dispatch = useDispatchStorePrincipal();
+
+  const {
+    systemReducer: { isOpenMultileg },
+  } = useStateStorePrincipal();
+
   const [hasOverflow, setHasOverflow] = useState(() => {
     const symbolsArray: string[] = [];
 
-    alert.structure.components.forEach((componentItem) => {
+    alertItem.structure.components.forEach((componentItem) => {
       symbolsArray.push(componentItem.stock.symbol);
     });
 
@@ -21,17 +39,44 @@ const AlertItem: React.FC<AlertItemProps> = ({ alert }) => {
     return symbols.length > 13;
   });
 
+  const [headerVisible, setHeaderVisible] = useState(false);
+
+  const handleHeaderVisibilityChange = useCallback(
+    () => setHeaderVisible((oldValue) => !oldValue),
+    [],
+  );
+
+  const handleClose = useCallback(async () => {
+    const data = await disableAlertAPI(alertItem.id);
+
+    console.log(data);
+  }, [alertItem]);
+
+  const handleSearch = useCallback(() => {
+    dispatch(
+      updateManySystemState({
+        multilegButtonsVisibility: false,
+        createAlertButtonVisibility: true,
+      }),
+    );
+
+    dispatchGlobal(atualizarDivKeyAction("multileg"));
+    dispatch(
+      abrirItemBarraLateralAction({ isOpenMultileg }, "isOpenMultileg", true),
+    );
+  }, [dispatch, dispatchGlobal, isOpenMultileg]);
+
   const formattedData = useMemo(() => {
     return {
-      price: formatarNumDecimal(alert.price, 2),
+      price: formatarNumDecimal(alertItem.price, 2),
       expiration: moment("20/10/2020 22:00:00", "DD/MM/YYYY HH:mm:ss").format(
         "DD/MM/YY",
       ),
     };
-  }, [alert]);
+  }, [alertItem]);
 
   const arrowColor = useMemo(() => {
-    switch (alert.param) {
+    switch (alertItem.param) {
       case "Bid":
         return "#207FA5";
       case "Ask":
@@ -39,35 +84,81 @@ const AlertItem: React.FC<AlertItemProps> = ({ alert }) => {
       default:
         return "#ccc";
     }
-  }, [alert.param]);
+  }, [alertItem.param]);
 
   useEffect(() => {
-    const positionItem = document.getElementById(`alertItem${alert.id}`);
+    const positionItem = document.getElementById(`alertItem${alertItem.id}`);
 
     if (positionItem) {
       setHasOverflow(positionItem.getBoundingClientRect().width > 198);
     }
-  }, [alert.id]);
+  }, [alertItem.id]);
 
-  return hasOverflow ? (
-    <>
-      {alert.structure.components.map((componentItem, index) => (
-        <div
+  return (
+    <div className="alertItemContainer">
+      <header className="itemHeader" style={{ opacity: headerVisible ? 1 : 0 }}>
+        <button onClick={handleSearch}>
+          <FiSearch size={20} stroke="#444" />
+        </button>
+
+        <button onClick={handleClose}>
+          <RiCloseCircleFill size={20} fill="#444" />
+        </button>
+      </header>
+      {hasOverflow ? (
+        <>
+          {alertItem.structure.components.map((componentItem, index) => (
+            <button
+              className="alertItem"
+              key={`${componentItem.stock.symbol}${index}`}
+              onClick={handleHeaderVisibilityChange}
+            >
+              <div>
+                <span style={{ color: "#ddd" }}>
+                  {componentItem.stock.symbol}
+                </span>
+                <span>
+                  {componentItem.qtty > 0
+                    ? `+${componentItem.qtty}`
+                    : componentItem.qtty}
+                </span>
+              </div>
+              <div>
+                <span>
+                  {formattedData.price}{" "}
+                  {alertItem.operator === "Less" ? (
+                    <FaLongArrowAltDown fill={arrowColor} />
+                  ) : (
+                    <FaLongArrowAltUp fill={arrowColor} />
+                  )}
+                </span>
+                <span>val: {formattedData.expiration}</span>
+              </div>
+            </button>
+          ))}
+        </>
+      ) : (
+        <button
           className="alertItem"
-          key={`${componentItem.stock.symbol}${index}`}
+          id={`alertItem${alertItem.id}`}
+          onClick={handleHeaderVisibilityChange}
         >
-          <div>
-            <span style={{ color: "#ddd" }}>{componentItem.stock.symbol}</span>
-            <span>
-              {componentItem.qtty > 0
-                ? `+${componentItem.qtty}`
-                : componentItem.qtty}
-            </span>
-          </div>
+          {alertItem.structure.components.map((componentItem, index) => (
+            <div key={`${componentItem.stock.symbol}${index}`}>
+              <span style={{ color: "#ddd" }}>
+                {componentItem.stock.symbol}
+              </span>
+              <span>
+                {componentItem.qtty > 0
+                  ? `+${componentItem.qtty}`
+                  : componentItem.qtty}
+              </span>
+            </div>
+          ))}
           <div>
             <span>
               {formattedData.price}{" "}
-              {alert.operator === "Less" ? (
+              {alertItem.operator === "Less" ? (
                 <FaLongArrowAltDown fill={arrowColor} />
               ) : (
                 <FaLongArrowAltUp fill={arrowColor} />
@@ -75,32 +166,8 @@ const AlertItem: React.FC<AlertItemProps> = ({ alert }) => {
             </span>
             <span>val: {formattedData.expiration}</span>
           </div>
-        </div>
-      ))}
-    </>
-  ) : (
-    <div className="alertItem" id={`alertItem${alert.id}`}>
-      {alert.structure.components.map((componentItem, index) => (
-        <div key={`${componentItem.stock.symbol}${index}`}>
-          <span style={{ color: "#ddd" }}>{componentItem.stock.symbol}</span>
-          <span>
-            {componentItem.qtty > 0
-              ? `+${componentItem.qtty}`
-              : componentItem.qtty}
-          </span>
-        </div>
-      ))}
-      <div>
-        <span>
-          {formattedData.price}{" "}
-          {alert.operator === "Less" ? (
-            <FaLongArrowAltDown fill={arrowColor} />
-          ) : (
-            <FaLongArrowAltUp fill={arrowColor} />
-          )}
-        </span>
-        <span>val: {formattedData.expiration}</span>
-      </div>
+        </button>
+      )}
     </div>
   );
 };
