18eca98f50acf9d0935b2225ba616a9bccbdeb85
Gabriel Alencar <gabriel_alencar2@hotmail.com>
2020-11-18 16:00:54 -0300

    Alterando mensagem de sucesso ao cancelar ordem em execução

--- src/constants/AlertaErros.ts
+++ src/constants/AlertaErros.ts
@@ -12,7 +12,8 @@ export const sucesso_criar_alerta = "Alerta criado com sucesso";
 export const erro_criar_alerta = "Falha ao criar alerta";
 export const sucesso_criar_posicao = "Posição criada com sucesso";
 export const erro_criar_posicao = "Falha ao criar posição";
-export const sucesso_cancelar_ordem = "Ordem cancelada com sucesso";
+export const sucesso_cancelar_ordem =
+  "Sua ordem está sendo cancelada. Acompanhe o Status para confirmar o Cancelamento";
 export const erro_cancelar_ordem = "Falha ao cancelar ordem";
 export const sucesso_finalizar_a_mercado = "Ordem finalizada com sucesso";
 export const erro_finalizar_a_mercado = "Falha ao finalizar ordem";
