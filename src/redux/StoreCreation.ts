0b3f9cb5c19b3b749e38a97306266544762eadc7
gabrielbezerra81 <gabriel_alencar2@hotmail.com>
2021-03-07 13:19:21 -0300

    Update StoreCreation.ts

--- src/redux/StoreCreation.ts
+++ src/redux/StoreCreation.ts
@@ -31,7 +31,7 @@ const rootPersistConfig = {
   key: "root",
   storage,
   stateReconciler: autoMergeLevel1,
-  whitelist: ["multilegReducer"], // "multiBoxReducer"
+  whitelist: [], // "multiBoxReducer"
 };
 
 const systemReducerPersistConfig = {
